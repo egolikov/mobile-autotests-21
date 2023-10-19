1590d5825eec4f98979a1c118dbf30e5810b6693
egolikov <panda_93a@mail.ru>
2023-10-19 22:16:19 +0600

    *upd tests

--- src/test/java/qa/guru/helpers/Browserstack.java
+++ src/test/java/qa/guru/helpers/Browserstack.java
@@ -13,7 +13,6 @@ public class Browserstack {
     public static String videoUrl(String sessionId) {
 
         String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
-
         return given()
                 .auth().basic(config.getUser(), config.getKey())
                 .when()
